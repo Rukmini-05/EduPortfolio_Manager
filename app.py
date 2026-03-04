aa77ed4be34ecbcf4655a121f437e679a39a7e2f
Rukmini-05 <rukminiwagmode2005@gmail.com>
2026-03-04 16:50:36 +0530

    Fixed Render port issue

--- app.py
+++ app.py
@@ -103,7 +103,11 @@ def logout():
     logout_user()
     return redirect(url_for("home"))
 
+import os
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
